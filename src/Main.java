276bd1c2157d83d8b216b31fd0abf2e15af0707f
wesdell <wesdell17@gmail.com>
2024-04-27 21:02:46 -0500

    Work with Chaleco methods.

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+/* Semana 1
+Clase 2
+
+Clase -> Extraer conceptos del mundo real a un dato primitivo. En un proceso
+denominado como Abstraccion.
+Atributos (campos) -> Caracteristicas primitivas de una clase.
+Objeto -> Concepto igual, atributos iguales con diferentes valores.
+
+UML -> Unified Modelling Language
+------------------- (Nombre de clase)
+Persona
+------------------- (Atributos)
+nombre
+edad
+genero
+------------------- (Metodos)
+respirar()
+comer()
+dormir()
+
+Clase 3
+
+Encapsulamiento -> Definir y manipular los atributos dentro de sus respectivas clases.
+*La unica manera de acceder/modificar a un atributo es mediante los metodos*
+
+Los atributos en general deben tener un nivel de acceso privado (puede ser publico).
+Los metodos en general deben tener un nivel de acceso publico (puede ser privado).
+*/
+
+public class Main {
+  public static void main(String[] args) {
+    Chaleco chaleco = new Chaleco();
+    chaleco.abrir();
+    System.out.println(chaleco);
+  }
+}
