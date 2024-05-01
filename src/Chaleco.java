b6e2ca8b99bd58c8e4dea2dd03d35dc4f031fa7e
wesdell <wesdell17@gmail.com>
2024-05-01 15:20:55 -0500

    New methods to get Chaleco state and to print Chaleco state.

--- src/Chaleco.java
+++ src/Chaleco.java
@@ -8,4 +8,13 @@ public class Chaleco {
   public void cerrar() {
     this.abierto = false;
   }
+
+  private String estaAbierto() {
+    return this.abierto ? "abierto" : "cerrado";
+  }
+
+  @Override
+  public String toString() {
+    return "El chaleco esta: " + this.estaAbierto();
+  }
 }
