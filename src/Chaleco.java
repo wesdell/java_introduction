915198a306ec78ac6a2aa93c150c9361a975ff54
wesdell <wesdell17@gmail.com>
2024-05-01 14:48:05 -0500

    Remove unnecessary code.

--- src/Chaleco.java
+++ src/Chaleco.java
@@ -2,11 +2,10 @@ public class Chaleco {
   private boolean abierto;
 
   public void abrir() {
-    abierto = true;
-    System.out.println("Test");
+    this.abierto = true;
   }
 
   public void cerrar() {
-    abierto = false;
+    this.abierto = false;
   }
 }
