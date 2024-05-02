dfc1e9cbc156b01aa969ffd07a1c5b0a8ee2aad1
wesdell <wesdell17@gmail.com>
2024-05-02 15:04:52 -0500

    New methods for Pocket class.

--- src/Pocket.java
+++ src/Pocket.java
@@ -2,12 +2,28 @@ public class Pocket {
   private boolean openPocket;
   private String content;
 
+  public Pocket() {
+    this.openPocket = false;
+  }
+
+  public void open() {
+    this.openPocket = true;
+  }
+
+  public void close() {
+    this.openPocket = false;
+  }
+
   public boolean isOpenPocket() {
     return this.openPocket;
   }
 
   public boolean isAvailable() {
-    return this.content == "";
+    return this.content == null;
+  }
+
+  public String getContent() {
+    return this.content;
   }
 
   public void save(String itemToSave) {
