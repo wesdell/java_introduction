ea7191e62ee7ead7cf795cb98aad593bcfe1b90c
wesdell <wesdell17@gmail.com>
2024-05-23 15:52:51 -0500

    Test code.

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,17 @@
 public class Main {
   public static void main(String[] args) {
-    Waistcoat waistcoat = new Waistcoat(); // Instancia de una clase
+    /*Waistcoat waistcoat = new Waistcoat();
     waistcoat.open();
     System.out.println(waistcoat.toString());
     waistcoat.close();
     System.out.println(waistcoat.toString());
     waistcoat.openPocket(1);
     waistcoat.save(1, "objeto");
-    System.out.println(waistcoat.getPocketsContent());
+    System.out.println(waistcoat.getPocketsContent());*/
+
+    Animal mammal = new Mammal();
+    Bird bird = new Bird();
+    System.out.println(mammal.breathe());
+    bird.fly();
   }
 }
