25b8ad7cd7b254c9d2dad4048b680f7c07d7e75d
wesdell <wesdell17@gmail.com>
2024-05-23 15:26:59 -0500

    Remove unnecessary comments.

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,3 @@
-/* Semana 1
-Clase 2
-
-Clase -> Extraer conceptos del mundo real a un dato primitivo. En un proceso
-denominado como Abstraccion.
-Atributos (campos) -> Caracteristicas primitivas de una clase.
-Objeto -> Concepto igual, atributos iguales con diferentes valores.
-
-UML -> Unified Modelling Language
-------------------- (Nombre de clase)
-Persona
-------------------- (Atributos)
-nombre
-edad
-genero
-------------------- (Metodos)
-respirar()
-comer()
-dormir()
-
-Clase 3
-
-Encapsulamiento -> Definir y manipular los atributos dentro de sus respectivas clases.
-*La unica manera de acceder/modificar a un atributo es mediante los metodos*
-
-Los atributos en general deben tener un nivel de acceso privado (puede ser publico).
-Los metodos en general deben tener un nivel de acceso publico (puede ser privado).
-*/
-
 public class Main {
   public static void main(String[] args) {
     Waistcoat waistcoat = new Waistcoat(); // Instancia de una clase
